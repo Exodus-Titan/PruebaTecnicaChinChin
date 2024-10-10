3734f03b09f5610ed679a1db9ecd26c2eb216a70
Carlos Ortega <carloseduardortega@gmail.com>
2024-10-10 17:25:14 -0400

    feat: :sparkles: Middleware

---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+
+class AppError extends Error {
+    public statusCode: number;
+    public isOperational: boolean;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = true; 
+        Error.captureStackTrace(this, this.constructor); 
+    }
+}
+
+
+const errorMiddleware = (err: AppError, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack); 
+    dotenv.config();
+    
+    res.status(err.statusCode || 500).json({
+        message: err.message || 'Ha ocurrido un error inesperado.',
+        error: process.env.NODE_ENV === 'development' ? err : {}, 
+    });
+};
+
+export { AppError, errorMiddleware };
